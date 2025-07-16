1f8f0cd21dbe423b02e632125c549386ee7c33b9
shenlink <2024133718@qq.com>
2025-07-16 14:18:57 +0800

    feat(main): 新增解析执行 holiya 文件的功能
    - 添加了对命令行参数的解析和处理
    - 实现了执行 holiya 文件的功能
    - 保留了原有的 REPL 模式

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"holiya/file"
 	"holiya/repl"
 	"os"
 )
 
 func main() {
 	// 直接运行，启动 repl 模式
-	repl.Start(os.Stdin, os.Stdout)
+	if len(os.Args) < 2 {
+		repl.Start(os.Stdin, os.Stdout)
+		return
+	}
+
+	// 执行 go run main.go filename.holiya 或 ./holira filename.holiya
+	err := file.ProcessFile(os.Args[1], os.Stdout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
 }
