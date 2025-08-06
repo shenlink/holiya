9f6615214acfc0e78e4d7ad0902f0a0970812a27
shenlink <2024133718@qq.com>
2025-08-06 23:31:50 +0800

    feat(parser): 添加 token 类型的优先级映射
    - 新增 precedences map，用于定义所有 token 类型对应的整数值
    - 添加了等号、不等号、小于、大于、加、减、除、乘、左括号、左中括号等 token 的优先级

--- parser/parser.go
+++ parser/parser.go
@@ -1,5 +1,9 @@
 package parser
 
+import (
+	"holiya/token"
+)
+
 const (
 	// 占位
 	_ int = iota
@@ -20,3 +24,17 @@ const (
 	// INDEX [，索引
 	INDEX
 )
+
+// 定义所有的token类型对应的整数值
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NEQ:      EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.DIV:      PRODUCT,
+	token.MUL:      PRODUCT,
+	token.LPAREN:   CALL,
+	token.LBRACKET: INDEX,
+}
