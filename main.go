1051f2d963db486f890e640e94fa2af46d2be892
shenlink <2024133718@qq.com>
2025-09-07 18:47:46 +0800

    feat(main): 添加帮助信息输出功能
    - 在 main 函数中增加参数检查，如果没有参数则输出帮助信息
    - 新增 printHelp 函数用于输出程序的使用说明

--- main.go
+++ main.go
@@ -7,10 +7,23 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		// 没有参数时，输出帮助信息
+		printHelp()
+		return
+	}
+
 	// 执行 go run main.go filename.holiya 或 ./holiya filename.holiya
 	err := file.ProcessFile(os.Args[1], os.Stdout)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
+}
+
+// printHelp 输出帮助信息
+func printHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("  ./holiya filename.holiya     Process the specified file")
+	fmt.Println("  go run main.go filename.holiya     Process the specified file")
 }
