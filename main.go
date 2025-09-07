267d939c667c63b2d0edf463df2324b86b661c93
shenlink <2024133718@qq.com>
2025-09-07 18:45:27 +0800

    refactor(main): 移除 REPL 功能

--- main.go
+++ main.go
@@ -3,17 +3,10 @@ package main
 import (
 	"fmt"
 	"holiya/file"
-	"holiya/repl"
 	"os"
 )
 
 func main() {
-	// 直接运行，启动 repl 模式
-	if len(os.Args) < 2 {
-		repl.Start(os.Stdin, os.Stdout)
-		return
-	}
-
 	// 执行 go run main.go filename.holiya 或 ./holiya filename.holiya
 	err := file.ProcessFile(os.Args[1], os.Stdout)
 	if err != nil {
